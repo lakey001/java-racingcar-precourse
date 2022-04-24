c5e95a64324cee4086eef4c09e822beaf7818282
sy001 <suyounlake@naver.com>
2022-04-24 20:29:03 +0900

    refactor: ParameterizedTest로 변경

--- src/test/java/racingcar/domain/RacingCarTest.java
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -1,22 +1,18 @@
 package racingcar.domain;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RacingCarTest {
-    @Test
-    void 전진_테스트(){
+    @ParameterizedTest(name = "전진 테스트 : 입력값 = {0}, 결과 = {1}")
+    @DisplayName("전진 테스트")
+    @CsvSource(value = {"4:1", "9:1", "0:0", "3:0"}, delimiter = ':')
+    void 전진_테스트(int value, int result){
         RacingCar racingCar = new RacingCar("test");
-        racingCar.move(4);
-        racingCar.move(9);
-        Assertions.assertThat(racingCar.getPosition()).isEqualTo(2);
-    }
-
-    @Test
-    void 정지_테스트(){
-        RacingCar racingCar = new RacingCar("test");
-        racingCar.move(0);
-        racingCar.move(3);
-        Assertions.assertThat(racingCar.getPosition()).isEqualTo(0);
+        racingCar.move(value);
+        Assertions.assertThat(racingCar.getPosition()).isEqualTo(result);
     }
 }
